beb4034eaca9bce1d51135ee575b62211694a0cf
hjzheld <dusen7774@naver.com>
2023-09-27 15:01:14 +0900

    ✨ 📝 댓글추가

--- articles/serializers.py
+++ articles/serializers.py
@@ -3,9 +3,14 @@ from .models import Article, Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    def get_user(self, obj):
+        return obj.user.email
+
     class Meta:
         model = Comment
-        fields = '__all__'
+        exclude = ["article",]
 
 
 class CommentCreateSerializer(serializers.ModelSerializer):
@@ -16,8 +21,9 @@ class CommentCreateSerializer(serializers.ModelSerializer):
 
 class ArticleSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
+    comment_set = CommentSerializer(many=True)
 
-    def get_user(serlf, obj):
+    def get_user(self, obj):
         return obj.user.email
 
     class Meta:
